58cafc1ed28beee7de4d60301de05b269e85bd7e
dominikocsofszki <10422001@student.vgu.edu.vn>
2022-12-17 10:18:29 +0700

    Adding DataLoader

--- train.py
+++ train.py
@@ -3,5 +3,18 @@ import torchvision
 import torchvision.datasets as datasets
 import torchvision.transforms as transforms
 
-dataset = datasets.MNIST(root='data/dataset',train=True,transform=transforms.ToTensor(),download=True)
-testset = datasets.MNIST(root='data/testset',transform=transforms.ToTensor(),download=True)
+import torch.utils.data
+
+# Variables
+BATCH_SIZE = 64
+
+# Downloading the dataset
+trainset = datasets.MNIST(root='data/dataset', train=True, transform=transforms.ToTensor(), download=True)
+testset = datasets.MNIST(root='data/testset', transform=transforms.ToTensor(), download=True)
+
+# Filter for only two classes #TODO Not sure yet if it is needed
+#TODO Do we need to normalize the data, or is it already done?
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=BATCH_SIZE, shuffle=True)
+testsetloader = torch.utils.data.DataLoader(testset, batch_size=BATCH_SIZE, shuffle=True)
+
+# Trainloader
