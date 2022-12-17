29b29ae09ce370842d84e7f069d0faf7b0900ce4
dominikocsofszki <10422001@student.vgu.edu.vn>
2022-12-17 09:36:20 +0700

    Adding model with nn.Module

--- model.py
+++ model.py
@@ -1,1 +1,18 @@
+import torch
+import torch.optim as optim
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class MyModel(nn.Module):
+    def __init__(self):
+        super().__init__()
+        # self.conv1 = nn.Conv2d(in_channels=28*28,kernel_size=(3,3),stride=1,padding=1,out_channels=28*28)
+        self.conv1 = nn.Conv2d(in_channels=28 * 28, kernel_size=(3, 3), stride=1, padding=1, out_channels=28 * 28)
+        self.conv2 = nn.Conv2d(in_channels=28 * 28, kernel_size=(3, 3), stride=1, padding=1, out_channels=28 * 28)
+        self.relu = nn.ReLU()
+
+    def forward(self, x):
+        x = self.relu(self.conv1(x))
+        return self.relu(self.conv2(x))
 
