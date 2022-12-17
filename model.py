0e1ddade49a3d2654e2e69284bac5d30fe6cb578
dominikocsofszki <10422001@student.vgu.edu.vn>
2022-12-17 11:08:37 +0700

    Still issues with conv, dims

--- model.py
+++ model.py
@@ -7,12 +7,15 @@ import torch.nn.functional as F
 class MyModel(nn.Module):
     def __init__(self):
         super().__init__()
-        # self.conv1 = nn.Conv2d(in_channels=28*28,kernel_size=(3,3),stride=1,padding=1,out_channels=28*28)
-        self.conv1 = nn.Conv2d(in_channels=28 * 28, kernel_size=(3, 3), stride=1, padding=1, out_channels=28 * 28)
-        self.conv2 = nn.Conv2d(in_channels=28 * 28, kernel_size=(3, 3), stride=1, padding=1, out_channels=28 * 28)
+        self.conv1 = nn.Conv2d(1, 4, kernel_size=3, stride=1, padding=1)
+        self.conv2 = nn.Conv2d(4, 10, kernel_size=3, stride=1, padding=1)
+
+
+        # self.conv1 = nn.Conv2d(in_channels=28, kernel_size=(3, 3), stride=1, padding=1, out_channels=28)
+        # self.conv2 = nn.Conv2d(in_channels=28 * 28, kernel_size=(3, 3), stride=1, padding=1, out_channels=28 * 28)
         self.relu = nn.ReLU()
 
     def forward(self, x):
+        # x = self.relu(self.conv1(x))
         x = self.relu(self.conv1(x))
         return self.relu(self.conv2(x))
-
--- train.py
+++ train.py
@@ -2,19 +2,49 @@ import torch
 import torchvision
 import torchvision.datasets as datasets
 import torchvision.transforms as transforms
-
+import torch.optim as optim
 import torch.utils.data
+import torch.nn as nn
+import model
 
 # Variables
 BATCH_SIZE = 64
-
+EPOCHS = 10
 # Downloading the dataset
 trainset = datasets.MNIST(root='data/dataset', train=True, transform=transforms.ToTensor(), download=True)
 testset = datasets.MNIST(root='data/testset', transform=transforms.ToTensor(), download=True)
 
 # Filter for only two classes #TODO Not sure yet if it is needed
-#TODO Do we need to normalize the data, or is it already done?
+# TODO Do we need to normalize the data, or is it already done?
+
+# Trainloader
 trainloader = torch.utils.data.DataLoader(trainset, batch_size=BATCH_SIZE, shuffle=True)
 testsetloader = torch.utils.data.DataLoader(testset, batch_size=BATCH_SIZE, shuffle=True)
 
-# Trainloader
+# Loss function and optimizer
+model = model.MyModel()
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.SGD(model.parameters(), lr=1e-3, momentum=0.9)
+
+for epoch in range(EPOCHS):
+    running_loss = 0.0
+
+    for i, data in enumerate(trainloader, 0):  # index = 0 could be deleted
+
+        X, y = data
+        X = X.permute(0,1,2,3)
+        print(X.shape)
+        optimizer.zero_grad()
+
+        pred = model(X)
+        loss = criterion(pred, y)
+
+        loss.backward()
+        optimizer.step()
+
+        running_loss += loss.item()
+
+        if i % 2000 == 1999:
+            print(f'[{epoch + 1}, {i + 1:5d}] loss: {running_loss / 2000:.3f}')
+            running_loss = 0.0
+print('finish!!!')
